e2a46ee8c3c675cafb6373e071ba1265b3a7f237
BOkungbure <babatundeokungbure@gmail.com>
2022-02-16 02:32:11 -0500

    Update ParchnPosey Data Exploration.sql

--- ParchnPosey Data Exploration.sql
+++ ParchnPosey Data Exploration.sql
@@ -383,10 +383,38 @@ SELECT id,
        account_id,
        standard_qty,
        DATE_TRUNC('month', occurred_at) AS month,
-       DENSE_RANK() OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS dense_rank,
-       SUM(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS sum_std_qty,
-       COUNT(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS count_std_qty,
-       AVG(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS avg_std_qty,
-       MIN(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS min_std_qty,
-       MAX(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS max_std_qty
+       DENSE_RANK() OVER (PARTITION BY account_id ) AS dense_rank,
+       SUM(standard_qty) OVER (PARTITION BY account_id ) AS sum_std_qty,
+       COUNT(standard_qty) OVER (PARTITION BY account_id ) AS count_std_qty,
+       AVG(standard_qty) OVER (PARTITION BY account_id ) AS avg_std_qty,
+       MIN(standard_qty) OVER (PARTITION BY account_id ) AS min_std_qty,
+       MAX(standard_qty) OVER (PARTITION BY account_id ) AS max_std_qty
 FROM orders
+
+
+SELECT id,
+       account_id,
+       DATE_TRUNC('year',occurred_at) AS year,
+       DENSE_RANK() OVER account_year_window AS dense_rank,
+       total_amt_usd,
+       SUM(total_amt_usd) OVER account_year_window AS sum_total_amt_usd,
+       COUNT(total_amt_usd) OVER account_year_window AS count_total_amt_usd,
+       AVG(total_amt_usd) OVER account_year_window AS avg_total_amt_usd,
+       MIN(total_amt_usd) OVER account_year_window AS min_total_amt_usd,
+       MAX(total_amt_usd) OVER account_year_window AS max_total_amt_usd
+FROM orders
+WINDOW account_year_window AS (PARTITION BY account_id ORDER BY DATE_TRUNC('year',occurred_at))
+
+
+SELECT occurred_at,
+       standard_sum,
+       LAG(standard_sum) OVER (ORDER BY standard_sum) AS lag,
+       LEAD(standard_sum) OVER (ORDER BY standard_sum) AS lead,
+       standard_sum - LAG(standard_sum) OVER (ORDER BY standard_sum) AS lag_difference,
+       LEAD(standard_sum) OVER (ORDER BY standard_sum) - standard_sum AS lead_difference
+FROM (
+SELECT occurred_at,
+       SUM(total_amt_usd) AS standard_sum
+  FROM orders 
+ GROUP BY 1
+ ) sub
