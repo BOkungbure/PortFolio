623e1bb431133f9c86d0b34ce3aa1c60e7e0dbc8
BOkungbure <babatundeokungbure@gmail.com>
2022-02-22 19:55:17 -0500

    Update for_loops_practice.py

--- for_loops_practice.py
+++ for_loops_practice.py
@@ -1,3 +1,6 @@
+from turtle import right
+
+
 sentence = ["the", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
 
 for word in sentence:
@@ -10,9 +13,40 @@ names = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
 user_names = []
 
 for i in range(len(names)):
-    name[i] = name.lower()
-    print(names)
+    names[i] = names[i].lower().replace(' ','_')
+print(names)
 
 for name in names:
     user_names.append(name.lower().replace(' ','_'))
     print(user_names)
+
+
+tokens = ['<greeting>', 'Hello World!', '</greeting>']
+count = 0
+tokens[-1]
+
+# write your for loop here
+for token in tokens:
+    if token[0] == '<' and token[-1] == '>':
+        count += 1
+
+print(count)
+
+
+items = ['first string', 'second string']
+html_str = '<ul>\n'
+
+for i in items:
+    html_str += '<li>{}</li>\n'.format(i)
+
+print(html_str)
+
+book_title =  ['great', 'expectations','the', 'adventures', 'of', 'sherlock','holmes','the','great','gasby','hamlet','adventures','of','huckleberry','fin']
+
+word_counter = {}
+for i in book_title:
+    if i not in book_title:
+        word_counter[i] = 1
+    else:
+        word_counter[i] += 1
+print(word_counter)
